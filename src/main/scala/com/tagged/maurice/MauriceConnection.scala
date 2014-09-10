1f3d2039ed0a86133a682872b99f7adb4984324f
Chris Johnson <cjohnson@tagged.com>
2014-09-10 14:24:40 -0700

    Added reading and writing of byte arrays

--- src/main/scala/com/tagged/maurice/MauriceConnection.scala
+++ src/main/scala/com/tagged/maurice/MauriceConnection.scala
@@ -1,6 +1,6 @@
 package com.tagged.maurice
 
-import org.apache.hadoop.hbase.client.{HTableInterface, HConnection}
+import org.apache.hadoop.hbase.client.{Get, Put, HTableInterface, HConnection}
 
 class MauriceConnection(connection: HConnection) {
 
@@ -10,6 +10,26 @@ class MauriceConnection(connection: HConnection) {
       block(table)
     } finally {
       table.close()
+    }
+  }
+
+  def writeBytes(tableName: String, rowKey: Array[Byte], columnFamily: String, columnName: String, value: Array[Byte]) = {
+    withTable(tableName) { table =>
+      val put = new Put(rowKey)
+      put.add(columnFamily.getBytes, columnName.getBytes, value)
+      table.put(put)
+      table.flushCommits()
+    }
+  }
+
+  def readBytes(tableName: String, rowKey: Array[Byte], columnFamily: String, columnName: String): Array[Byte] = {
+    withTable(tableName) { table =>
+      val rawColumnFamily = columnFamily.getBytes
+      val rawColumnName = columnName.getBytes
+      val get = new Get(rowKey)
+      get.addColumn(rawColumnFamily, rawColumnName)
+      val result = table.get(get)
+      result.getValue(columnFamily.getBytes, columnName.getBytes)
     }
   }
 
--- src/test/scala/com/tagged/maurice/MauriceConnectionSpec.scala
+++ src/test/scala/com/tagged/maurice/MauriceConnectionSpec.scala
@@ -12,8 +12,8 @@ import org.specs2.mutable.Specification
 class MauriceConnectionSpec extends Specification {
 
   val tableName = "MauriceConnectionSpec"
-  val family1 = "family1".getBytes
-  val family2 = "family2".getBytes
+  val family1 = "family1"
+  val family2 = "family2"
 
   val configuration = HBaseConfiguration.create()
   scala.util.Properties.propOrNone("hbase.zookeeper.quorum") match {
@@ -26,22 +26,33 @@ class MauriceConnectionSpec extends Specification {
 
   "withTable" should {
 
-    "let us write and read using the HTableInterface primitives" in {
+    "let us write and read using HTableInterface primitives" in {
       val rowkey = System.nanoTime().toString.getBytes
       val qualifier = "primitiveReadWriteTest".getBytes
       val writeValue = System.nanoTime().toString.getBytes
 
       val readResult = maurice.withTable(tableName) { table =>
         val put = new Put(rowkey)
-        put.add(family1, qualifier, writeValue)
+        put.add(family1.getBytes, qualifier, writeValue)
         table.put(put)
         table.flushCommits()
 
         val get = new Get(rowkey)
-        get.addColumn(family1, qualifier)
+        get.addColumn(family1.getBytes, qualifier)
         val result = table.get(get)
-        result.getValue(family1, qualifier)
+        result.getValue(family1.getBytes, qualifier)
       }
+
+      readResult must beEqualTo(writeValue)
+    }
+
+    "let us write and read byte arrays" in {
+      val rowKey = System.nanoTime().toString.getBytes
+      val columnName = "byteArrayReadWriteTest"
+      val writeValue = System.nanoTime().toString.getBytes
+
+      maurice.writeBytes(tableName, rowKey, family1, columnName, writeValue)
+      val readResult = maurice.readBytes(tableName, rowKey, family1, columnName)
 
       readResult must beEqualTo(writeValue)
     }
